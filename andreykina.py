809a25bc4f684aa71304415fb8f3f66472ecd116
pa1weq <114472646+pa1weq@users.noreply.github.com>
2022-09-27 15:59:32 +0700

    Delete andreykina.py

--- andreykina.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-connection = sqlite3.connect('shows.db')
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Shows
-              (Title TEXT, Director TEXT, Year INT)''')
-connection.commit()
-connection.close()
